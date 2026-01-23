d00f68591c9bfcecf6e7f304167dd3542001da87
Xander-Mosley <XanderDMosley.Engineer@gmail.com>
2026-01-23 11:28:04 -0600

    Updating imudifferentiating.py (problems appeared)

--- ros2_sid/ros2_sid/data_parser.py
+++ ros2_sid/ros2_sid/data_parser.py
@@ -375,7 +375,7 @@ def main(bag_file, topics_to_extract, output_directory):
     close(db_connection)
 
 if __name__ == "__main__":
-    bag_file = '/develop_ws/bag_files/2026-01-23_BugHunt3/rosbag2_2026_01_23-17_06_15_0.db3'
+    bag_file = '/develop_ws/bag_files/2026-01-23_BugHunt4/rosbag2_2026_01_23-17_23_16_0.db3'
     
     topics_to_extract = {
         # '/mavros/imu/data': 'imu',
